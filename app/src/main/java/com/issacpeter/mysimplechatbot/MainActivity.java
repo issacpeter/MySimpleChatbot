7496622f1752f2dc08d9da411feea2c9225f1a27
issacpc <issacpc3@gmail.com>
2017-10-25 17:58:06 +0530

    code for alice added

--- app/src/main/java/com/issacpeter/mysimplechatbot/MainActivity.java
+++ app/src/main/java/com/issacpeter/mysimplechatbot/MainActivity.java
@@ -1,8 +1,12 @@
 package com.issacpeter.mysimplechatbot;
 
+import android.content.res.AssetManager;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -10,9 +14,20 @@ import android.widget.ListView;
 
 import com.issacpeter.mysimplechatbot.data.ChatMessage;
 
+import org.alicebot.ab.AIMLProcessor;
 import org.alicebot.ab.Bot;
 import org.alicebot.ab.Chat;
+import org.alicebot.ab.Graphmaster;
+import org.alicebot.ab.MagicBooleans;
+import org.alicebot.ab.MagicStrings;
+import org.alicebot.ab.PCAIMLProcessorExtension;
+import org.alicebot.ab.Timer;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -43,11 +58,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String message = mEditTextMessage.getText().toString();
+                //bot
+                String response = chat.multisentenceRespond(mEditTextMessage.getText().toString());
+                if (TextUtils.isEmpty(message)) {
+                    return;
+                }
                 sendMessage(message);
+                mimicOtherMessage(response);
                 mEditTextMessage.setText("");
                 mListView.setSelection(mAdapter.getCount() - 1);
             }
         });
+
+        prepareAlice();
     }
 
     private void sendMessage(String message) {
@@ -72,5 +95,77 @@ public class MainActivity extends AppCompatActivity {
     private void mimicOtherMessage() {
         ChatMessage chatMessage = new ChatMessage(null, false, true);
         mAdapter.add(chatMessage);
+    }
+
+    public void prepareAlice(){
+        //checking SD card availablility
+        boolean a = isSDCARDAvailable();
+//receiving the assets from the app directory
+        AssetManager assets = getResources().getAssets();
+        File jayDir = new File(Environment.getExternalStorageDirectory().toString() + "/hari/bots/Hari");
+        boolean b = jayDir.mkdirs();
+        Log.d("Chatbot " , "prepareAlice: "+jayDir.getAbsolutePath());
+        if (jayDir.exists()) {
+            //Reading the file
+            try {
+                for (String dir : assets.list("Hari")) {
+                    File subdir = new File(jayDir.getPath() + "/" + dir);
+                    boolean subdir_check = subdir.mkdirs();
+                    for (String file : assets.list("Hari/" + dir)) {
+                        File f = new File(jayDir.getPath() + "/" + dir + "/" + file);
+                        if (f.exists()) {
+                            continue;
+                        }
+                        InputStream in = null;
+                        OutputStream out = null;
+                        in = assets.open("Hari/" + dir + "/" + file);
+                        out = new FileOutputStream(jayDir.getPath() + "/" + dir + "/" + file);
+                        //copy file from assets to the mobile's SD card or any secondary memory
+                        copyFile(in, out);
+                        in.close();
+                        in = null;
+                        out.flush();
+                        out.close();
+                        out = null;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+//get the working directory
+        MagicStrings.root_path = Environment.getExternalStorageDirectory().toString() + "/hari";
+        System.out.println("Working Directory = " + MagicStrings.root_path);
+        AIMLProcessor.extension =  new PCAIMLProcessorExtension();
+//Assign the AIML files to bot for processing
+        bot = new Bot("Hari", MagicStrings.root_path, "chat");
+        chat = new Chat(bot);
+        String[] args = null;
+        mainFunction(args);
+    }
+
+    //check SD card availability
+    public static boolean isSDCARDAvailable(){
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)? true :false;
+    }
+    //copying the file
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
+    }
+    //Request and response of user and the bot
+    public static void mainFunction (String[] args) {
+        MagicBooleans.trace_mode = false;
+        System.out.println("trace mode = " + MagicBooleans.trace_mode);
+        Graphmaster.enableShortCuts = true;
+        Timer timer = new Timer();
+        String request = "Hello.";
+        String response = chat.multisentenceRespond(request);
+
+        System.out.println("Human: "+request);
+        System.out.println("Robot: " + response);
     }
 }
